17cd6e02046e5de106074c10783d1dff6c64d92b
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-07-06 15:55:05 -0400

    added another API to read current day weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,30 @@
 
 var apiKey = "11313c3687393e3de0575eed3e2d8890";
 
-var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}`;
+var apiUrlFiveDay = "https://api.openweathermap.org/data/2.5/forecast";
 
-function getWeatherPromise(lat, lon) {
-  return fetch(`${apiUrl}&lat=${lat}&lon=${lon}`, {
+var apiUrlCurrent = "https://api.openweathermap.org/data/2.5/weather";
+
+function getFiveDayWeatherPromise(lat, lon) {
+  return fetch(`${apiUrlFiveDay}?appid=${apiKey}&lat=${lat}&lon=${lon}`, {
     method: 'GET', //GET is the default.
     credentials: 'same-origin', // include, *same-origin, omit
     redirect: 'follow', // manual, *follow, error
   });
 }
 
-getWeatherPromise("43.741667", "-79.373333")
-  .then(function (data) {
+function getCurrentWeatherPromise(lat, lon) {
+  return fetch(`${apiUrlCurrent}?appid=${apiKey}&lat=${lat}&lon=${lon}`, {
+    method: 'GET', //GET is the default.
+    credentials: 'same-origin', // include, *same-origin, omit
+    redirect: 'follow', // manual, *follow, error
+  });
+}
+
+getCurrentWeatherPromise("43.741667", "-79.373333")
+  .then(function (response) {
+    // This method returns a Promise as well! So we will "chain" it by returning it 
+    return response.json(); 
+  }).then( data => {
     console.log(data);
-  })
+  });
