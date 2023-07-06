11438c2d885fc18dd813f517dec11f8d5fbeaab3
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-07-06 16:00:58 -0400

    use city name in API request instead of lat/lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,26 +7,34 @@ var apiUrlFiveDay = "https://api.openweathermap.org/data/2.5/forecast";
 
 var apiUrlCurrent = "https://api.openweathermap.org/data/2.5/weather";
 
-function getFiveDayWeatherPromise(lat, lon) {
-  return fetch(`${apiUrlFiveDay}?appid=${apiKey}&lat=${lat}&lon=${lon}`, {
+function getFiveDayWeatherPromise(cityName) {
+  return fetch(`${apiUrlFiveDay}?appid=${apiKey}&q=${cityName}`, {
     method: 'GET', //GET is the default.
     credentials: 'same-origin', // include, *same-origin, omit
     redirect: 'follow', // manual, *follow, error
   });
 }
 
-function getCurrentWeatherPromise(lat, lon) {
-  return fetch(`${apiUrlCurrent}?appid=${apiKey}&lat=${lat}&lon=${lon}`, {
+function getCurrentWeatherPromise(cityName) {
+  return fetch(`${apiUrlCurrent}?appid=${apiKey}&q=${cityName}`, {
     method: 'GET', //GET is the default.
     credentials: 'same-origin', // include, *same-origin, omit
     redirect: 'follow', // manual, *follow, error
   });
 }
 
-getCurrentWeatherPromise("43.741667", "-79.373333")
-  .then(function (response) {
-    // This method returns a Promise as well! So we will "chain" it by returning it 
-    return response.json(); 
-  }).then( data => {
-    console.log(data);
-  });
+function getWeatherForCity(cityName) {
+  var cityHeadingElement = document.querySelector("#city h2");
+  cityHeadingElement.textContent = cityName;
+
+  getCurrentWeatherPromise(cityName)
+    .then(function (response) {
+      // This method returns a Promise as well! So we will "chain" it by returning it 
+      return response.json(); 
+    }).then( data => {
+      console.log(data);
+    });
+}
+
+getWeatherForCity("Toronto");
+
