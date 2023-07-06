fc284f39b8ad6acf8c76b362fffebeeff9e9f9e8
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-07-06 14:09:21 -0400

    started javascript and tested api with our key

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,17 @@
 
 var apiKey = "11313c3687393e3de0575eed3e2d8890";
 
+var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}`;
+
+function getWeatherPromise(lat, lon) {
+  return fetch(`${apiUrl}&lat=${lat}&lon=${lon}`, {
+    method: 'GET', //GET is the default.
+    credentials: 'same-origin', // include, *same-origin, omit
+    redirect: 'follow', // manual, *follow, error
+  });
+}
+
+getWeatherPromise("43.741667", "-79.373333")
+  .then(function (data) {
+    console.log(data);
+  })
